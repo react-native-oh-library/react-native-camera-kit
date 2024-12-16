81c855e7ce036fda30af8a6b292f2505bd5d9364
carry <duanshao1@h-partners.com>
2024-12-16 16:09:04 +0800

    chore：开源文件五项核查

--- harmony/camera_kit/src/main/ets/utils/PermissionUtils.ts
+++ harmony/camera_kit/src/main/ets/utils/PermissionUtils.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import abilityAccessCtrl, { Context, PermissionRequestResult } from '@ohos.abilityAccessCtrl';
 import { Permissions } from '@ohos.abilityAccessCtrl';
 import bundleManager from '@ohos.bundle.bundleManager';
